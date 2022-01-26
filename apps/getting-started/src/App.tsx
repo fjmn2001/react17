f326d03c5e8e121d0a2c09b61a3c7c2cc05e251c
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-01-26 11:11:19 -0400

    feat: getting-started v1. Ref sn

--- apps/getting-started/src/App.tsx
+++ apps/getting-started/src/App.tsx
@@ -1,7 +1,23 @@
+interface User {
+    firstName: string
+    lastName: string
+}
+
 export default function App() {
+    function formatName(user: User): string {
+        return user.firstName + ' ' + user.lastName;
+    }
+
+    const user: User = {
+        firstName: 'Harper',
+        lastName: 'Perez'
+    };
+
     return (
         <>
-            <h1>Hi there!</h1>
+            <h1>
+                Hello, { formatName(user) }!
+            </h1>
         </>
     );
 }
