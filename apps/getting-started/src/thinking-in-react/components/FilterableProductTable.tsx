ff7e487b52a7510ab37e4871b2bd5cf2c0941acc
🚀 Francisco Marcano <fjmn2001@gmail.com>
2022-02-04 18:34:20 -0400

    feat: thinking in React v2. Ref sn

--- apps/getting-started/src/thinking-in-react/components/FilterableProductTable.tsx
+++ apps/getting-started/src/thinking-in-react/components/FilterableProductTable.tsx
@@ -1,12 +1,31 @@
 import { Product } from "../repository/ProductsRepository";
 import SearchBar from "./SearchBar";
 import ProductTable from "./ProductTable";
+import { useState } from "react";
 
 const FilterableProductTable = ({products}: { products: Product[] }) => {
+    const [filterText, setFilterText] = useState('');
+    const [inStockOnly, setInStockOnly] = useState(false);
+
+    const handleFilerTextChange = (value: string) => {
+        setFilterText(value)
+    }
+
+    const handleInStockOnlyChange = (value: boolean) => {
+        setInStockOnly(value)
+    }
+
     return (
         <div>
-            <SearchBar />
-            <ProductTable products={ products } />
+            <SearchBar
+                onFilerTextChange={ handleFilerTextChange }
+                onInStockOnlyChange={ handleInStockOnlyChange }
+                filterText={ filterText }
+                inStockOnly={ inStockOnly } />
+            <ProductTable
+                products={ products }
+                filterText={ filterText }
+                inStockOnly={ inStockOnly } />
         </div>
     );
 }
--- apps/getting-started/src/thinking-in-react/components/SearchBar.tsx
+++ apps/getting-started/src/thinking-in-react/components/SearchBar.tsx
@@ -1,9 +1,26 @@
-const SearchBar = () => {
+import { ChangeEvent } from "react";
+
+interface SearchBarProps {
+    filterText: string
+    inStockOnly: boolean
+    onFilerTextChange: (value: string) => void
+    onInStockOnlyChange: (value: boolean) => void
+}
+
+const SearchBar = ({filterText, inStockOnly, onFilerTextChange, onInStockOnlyChange}: SearchBarProps) => {
+    const handleText = (e: ChangeEvent<HTMLInputElement>) => {
+        onFilerTextChange(e.target.value);
+    }
+
+    const handleInStockOnly = (e: ChangeEvent<HTMLInputElement>) => {
+        onInStockOnlyChange(e.target.checked);
+    }
+
     return (
         <form>
-            <input type="text" placeholder="Search..." />
+            <input type="text" placeholder="Search..." value={ filterText } onChange={ handleText } />
             <p>
-                <input type="checkbox" />
+                <input type="checkbox" checked={ inStockOnly } onChange={ handleInStockOnly } />
                 { ' ' }
                 Only show products in stock
             </p>
